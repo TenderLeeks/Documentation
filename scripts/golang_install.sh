277103bcae58154e4436d2e6c4cd7a114b7e94ae
yushuai <ken200930@126.com>
2022-02-24 20:41:35 +0800

    up

---
+++ scripts/golang_install.sh
@@ -0,0 +1,62 @@
+#!/usr/bin/env bash
+set -e
+
+VERSION="${2:-1.17.3}"
+DIR="${3:-${HOME}}"
+
+echo "GoLang version is ${VERSION}."
+echo "The GoLang installation directory is ${DIR}"
+echo
+
+uninstall() {
+  if [ -f "/etc/profile.d/go.sh" ]; then
+    rm -f /etc/profile.d/go.sh
+    echo "rm /etc/profile.d/go.sh"
+  else
+    echo "not found /etc/profile.d/go.sh"
+  fi
+
+  if [ -f "go${VERSION}.linux-amd64.tar.gz" ]; then
+    rm -f go"${VERSION}".linux-amd64.tar.gz
+    echo "rm go${VERSION}.linux-amd64.tar.gz"
+  else
+    echo "not found /tmp/go${VERSION}.linux-amd64.tar.gz"
+  fi
+
+  if [ -d "${DIR}/go" ]; then
+    rm -rf "${DIR}"/go
+    echo "rm ${DIR}/go"
+  else
+    echo "not found ${DIR}/go"
+  fi
+}
+
+install() {
+  uninstall
+  echo
+  cd /tmp
+  wget https://golang.google.cn/dl/go"${VERSION}".linux-amd64.tar.gz
+  tar -zxf go"${VERSION}".linux-amd64.tar.gz -C "${DIR}"
+  tee /etc/profile.d/go.sh <<EOF
+export GOROOT=${DIR}/go
+export PATH=\$GOROOT/bin:\$PATH
+export GOPATH=${DIR}/go/go_path
+export GOPROXY=https://goproxy.cn
+EOF
+  source /etc/profile
+  go version
+  [ $? -eq 0 ] && echo "Successful installation"
+}
+
+
+case "$1" in
+    install)
+        install
+        ;;
+    uninstall)
+        uninstall
+        ;;
+    *)
+        echo "Usage: $0 {install|uninstall}"
+        exit 2
+esac
--- scripts/node_install.sh
+++ scripts/node_install.sh
@@ -1,23 +1,71 @@
-#!/bin/bash
-
+#!/usr/bin/env bash
 set -e
 
-NODE_VERSION="${1:-v16.9.1}"
-INStALL_DIR="${2:-/opt}"
+VERSION="${2:-v16.9.1}"
+DIR="${3:-${HOME}}"
 
-wget -q https://nodejs.org/dist/"${NODE_VERSION}"/node-"${NODE_VERSION}"-linux-x64.tar.gz -O /tmp/node-"${NODE_VERSION}"-linux-x64.tar.gz
+echo "Node version is ${VERSION}."
+echo "The Node installation directory is ${DIR}"
+echo
 
-tar -zxf /tmp/node-"${NODE_VERSION}"-linux-x64.tar.gz -C "${INStALL_DIR}"
+uninstall() {
+  if [ -f "/etc/profile.d/node.sh" ]; then
+    rm -f /etc/profile.d/node.sh
+    echo "rm /etc/profile.d/node.sh"
+  else
+    echo "not found /etc/profile.d/node.sh"
+  fi
 
-mv "${INStALL_DIR}"/node-"${NODE_VERSION}"-linux-x64 "${INStALL_DIR}"/node-"${NODE_VERSION}"
+  if [ -f "/tmp/node-${VERSION}-linux-x64.tar.gz" ]; then
+    rm -f /tmp/node-"${VERSION}"-linux-x64.tar.gz
+    echo "rm /tmp/node-${VERSION}-linux-x64.tar.gz"
+  else
+    echo "not found /tmp/node-${VERSION}-linux-x64.tar.gz"
+  fi
 
-tee /etc/profile.d/node.sh <<EOF
-export NODE=/opt/node-${NODE_VERSION}
+  if [ -d "${DIR}/node-${VERSION}" ]; then
+    rm -rf "${DIR}"/node-"${VERSION}"
+    echo "rm ${DIR}/node-${VERSION}"
+  else
+    echo "not found ${DIR}/node-${VERSION}"
+  fi
+}
+
+install() {
+  uninstall
+  echo
+  cd /tmp
+  wget https://nodejs.org/dist/"${VERSION}"/node-"${VERSION}"-linux-x64.tar.gz
+  tar -zxf node-"${VERSION}"-linux-x64.tar.gz -C "${DIR}"
+  mv "${DIR}"/node-"${VERSION}"-linux-x64 "${DIR}"/node-"${VERSION}"
+  tee /etc/profile.d/node.sh <<EOF
+export NODE=${DIR}/node-${VERSION}
 export PATH=\$NODE/bin:\$PATH
 EOF
+  source /etc/profile
+  node -v
+  [ $? -eq 0 ] && echo "Successful installation"
+}
 
-source /etc/profile
-node -v
+install_all() {
+  install
+  npm install -g cnpm --registry=https://registry.npm.taobao.org
+  npm install yarn -g
+  npm install pm2 -g
+  npm install apidoc -g
+}
 
-npm install -g cnpm --registry=https://registry.npm.taobao.org
-cnpm install yarn -g
+case "$1" in
+    install)
+        install
+        ;;
+    uninstall)
+        uninstall
+        ;;
+    install_all)
+        install_all
+        ;;
+    *)
+        echo "Usage: $0 {install|install_all|uninstall}"
+        exit 2
+esac
